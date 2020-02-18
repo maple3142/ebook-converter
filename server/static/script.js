c3c52c2251e10bd9ba3b18754176e17f2fb657d1
maple3142 <kirby741852963@gmail.com>
2020-02-18 10:56:18 +0800

    add simple ga

--- server/static/script.js
+++ server/static/script.js
@@ -9,3 +9,16 @@ for (const select of selects) {
 		select.value = localStorage.getItem(select.id)
 	}
 }
+
+const uid = localStorage.getItem('uid') || Math.random() + '.' + Math.random()
+localStorage.setItem('uid', uid)
+navigator.sendBeacon(
+	'https://www.google-analytics.com/collect',
+	new URLSearchParams({
+		v: 1,
+		tid: 'UA-108821694-3',
+		cid: uid,
+		t: 'pageview',
+		dp: encodeURIComponent(location.pathname)
+	}).toString()
+)
