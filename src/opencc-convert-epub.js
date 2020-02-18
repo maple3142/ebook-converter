9bcaa47b3b3c6e30017e88bec7cfb3c755c191a0
maple3142 <kirby741852963@gmail.com>
2020-02-18 10:46:09 +0800

    update epub converting logic

--- src/opencc-convert-epub.js
+++ src/opencc-convert-epub.js
@@ -2,6 +2,7 @@ const path = require('path')
 const fs = require('fs-extra')
 const tmp = require('tmp-promise')
 const unzipper = require('unzipper')
+const globby = require('globby')
 const cvtFile = require('./opencc-convert-text')
 const zipDir = require('./zip-dir')
 
@@ -22,26 +23,9 @@ module.exports = async (filePath, config) => {
 			.on('close', res)
 			.on('error', rej)
 	})
-	const textDir = path.join(dir, './OEBPS/Text')
-	const hasTextDir = await fs.exists(textDir)
-	if (!hasTextDir) {
-		throw new Error('The file is not a EPUB file.')
-	}
-	const files = (await fs.readdir(textDir))
-		.filter(name => name.endsWith('.xhtml'))
-		.map(name => path.join(textDir, name))
-	const toc1 = path.join(dir, './toc.ncx')
-	if (await fs.exists(toc1)) {
-		files.push(toc1)
-	}
-	const toc2 = path.join(dir, './OEBPS/toc.ncx')
-	if (await fs.exists(toc2)) {
-		files.push(toc2)
-	}
-	const meta = path.join(dir, './OEBPS/content.opf')
-	if (await fs.exists(meta)) {
-		files.push(meta)
-	}
+	const files = (
+		await globby('**/*.{htm,html,xhtml,ncx,opf}', { cwd: dir })
+	).map(f => path.join(dir, f))
 	await Promise.all(files.map(f => cvtFile(f, { type: config.type })))
 	await zipDir(dir, config.dest || filePath)
 	await cleanup()
