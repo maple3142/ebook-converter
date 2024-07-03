3cce9ae7271447b9a24c5d2ec378a70b742e9f9a
maple3142 <kirby741852963@gmail.com>
2024-07-04 00:51:19 +0800

    refactor

--- server/index.js
+++ server/index.js
@@ -20,6 +20,9 @@ const upload = multer({
 		// fix: https://github.com/expressjs/multer/pull/1102
 		file.originalname = Buffer.from(file.originalname, 'latin1').toString('utf8')
 		cb(null, true)
+	},
+	limits: {
+		fileSize: 64 * 1024 * 1024 // 64MB
 	}
 })
 
@@ -49,15 +52,17 @@ const downloadFile = async (res, fileName, filePath) => {
 	)
 }
 const files = Object.create(null)
-function addDeleteTask(fileId) {
-	console.log(`Adding ${fileId} for deletion...`)
+function performDelete(file) {
+	console.log(`Deleting ${file.id} ...`)
+	delete files[file.id]
+	fs.unlink(file.path).catch(err => {
+		console.log(`Deleting ${file.id} error: ${err}`)
+	})
+}
+function addDeleteTask(file) {
+	console.log(`Schedule ${file.id} for deletion...`)
 	setTimeout(() => {
-		console.log(`Deleting ${fileId} ...`)
-		const file = files[fileId]
-		delete files[fileId]
-		fs.unlink(file.path).catch(err => {
-			console.log(`Deleting ${fileId} error: ${err}`)
-		})
+		performDelete(file)
 	}, DELETE_DELAY)
 }
 app.get('/files/:id', (req, res) => {
@@ -78,107 +83,75 @@ app.get('/info/:id', (req, res) => {
 	}
 	res.render('info', { id: req.params.id, file })
 })
-app.post('/opencc-convert-epub', upload.single('file'), async (req, res) => {
-	if (!req.file || req.file.mimetype !== 'application/epub+zip') {
-		return res.send('檔案並非 epub 類型')
+const handleFileMiddleware = type => [
+	upload.single('file'),
+	(req, res, next) => {
+		if (!req.file || req.file.mimetype !== type) {
+			if (req.file) {
+				fs.unlink(req.file.path, () => {})
+			}
+			return res.type('text').send(`檔案類型並非 ${type}`)
+		}
+		const fileId = req.file.filename
+		req.file = files[fileId] = {
+			originalName: req.file.originalname,
+			path: req.file.path,
+			generated: Date.now(),
+			done: false,
+			id: fileId
+		}
+		res.redirect(`/info/${fileId}`)
+		next()
 	}
-	const fileId = req.file.filename
-	const file = (files[fileId] = {
-		originalName: req.file.originalname,
-		path: req.file.path,
-		generated: Date.now(),
-		done: false
-	})
-	res.redirect(`/info/${fileId}`)
-	await openccCvtEpub(file.path, {
+]
+const handleTaskCompletion = (file, task) =>
+	task
+		.then(() => {
+			console.log(`Conversion of ${file.id} done`)
+			file.done = true
+			file.generated = Date.now()
+			addDeleteTask(file)
+		})
+		.catch(() => {
+			console.log(`Conversion of ${file.id} failed`)
+			file.error = true
+			setTimeout(() => {
+				performDelete(file)
+			}, 10 * 1000) // so that the user can see the error message (by redirecting to /info/:id)
+		})
+app.post('/opencc-convert-epub', handleFileMiddleware('application/epub+zip'), (req, res) => {
+	const { file } = req
+	const task = openccCvtEpub(file.path, {
 		type: {
 			from: req.body['type-from'],
 			to: req.body['type-to']
 		}
 	})
-		.then(() => {
-			file.done = true
-			file.generated = Date.now()
-		})
-		.catch(() => {
-			file.error = true
-		})
-	addDeleteTask(fileId)
+	handleTaskCompletion(file, task)
 })
-app.post('/opencc-convert-txt', upload.single('file'), async (req, res) => {
-	if (!req.file || req.file.mimetype !== 'text/plain') {
-		return res.send('檔案並非 txt 類型')
-	}
-	const fileId = req.file.filename
-	const file = (files[fileId] = {
-		originalName: req.file.originalname,
-		path: req.file.path,
-		generated: Date.now(),
-		done: false
-	})
-	res.redirect(`/info/${fileId}`)
-	await openccCvtText(file.path, {
+app.post('/opencc-convert-txt', handleFileMiddleware('text/plain'), (req, res) => {
+	const { file } = req
+	const task = openccCvtText(file.path, {
 		type: {
 			from: req.body['type-from'],
 			to: req.body['type-to']
 		}
 	})
-		.then(() => {
-			file.done = true
-			file.generated = Date.now()
-		})
-		.catch(() => {
-			file.error = true
-		})
-	addDeleteTask(fileId)
+	handleTaskCompletion(file, task)
 })
-app.post('/zhc-convert-epub', upload.single('file'), async (req, res) => {
-	if (!req.file || req.file.mimetype !== 'application/epub+zip') {
-		return res.send('檔案並非 epub 類型')
-	}
-	const fileId = req.file.filename
-	const file = (files[fileId] = {
-		originalName: req.file.originalname,
-		path: req.file.path,
-		generated: Date.now(),
-		done: false
-	})
-	res.redirect(`/info/${fileId}`)
-	await zhcCvtEpub(file.path, {
+app.post('/zhc-convert-epub', handleFileMiddleware('application/epub+zip'), (req, res) => {
+	const { file } = req
+	const task = zhcCvtEpub(file.path, {
 		type: req.body.type
 	})
-		.then(() => {
-			file.done = true
-			file.generated = Date.now()
-		})
-		.catch(() => {
-			file.error = true
-		})
-	addDeleteTask(fileId)
+	handleTaskCompletion(file, task)
 })
-app.post('/zhc-convert-txt', upload.single('file'), async (req, res) => {
-	if (!req.file || req.file.mimetype !== 'text/plain') {
-		return res.send('檔案並非 txt 類型')
-	}
-	const fileId = req.file.filename
-	const file = (files[fileId] = {
-		originalName: req.file.originalname,
-		path: req.file.path,
-		generated: Date.now(),
-		done: false
-	})
-	res.redirect(`/info/${fileId}`)
-	await zhcCvtText(file.path, {
+app.post('/zhc-convert-txt', handleFileMiddleware('application/epub+zip'), (req, res) => {
+	const { file } = req
+	const task = zhcCvtText(file.path, {
 		type: req.body.type
 	})
-		.then(() => {
-			file.done = true
-			file.generated = Date.now()
-		})
-		.catch(() => {
-			file.error = true
-		})
-	addDeleteTask(fileId)
+	handleTaskCompletion(file, task)
 })
 
 const PORT = process.env.PORT || 8763
